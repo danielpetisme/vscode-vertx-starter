724168a8abb5f7420a53db45a7c7115c6ff78b3b
Daniel Petisme <daniel.petisme@gmail.com>
2019-05-15 00:41:30 +0200

    Vertx dependencies + sanity checks

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,15 @@ import * as vscode from 'vscode';
 import * as path from "path";
 import * as unzipper from 'unzipper';
 import * as request from 'request-promise';
+import { QuickPickItem } from 'vscode';
 
 const API_URL = "https://start.vertx.io";
 const LANGUAGES = [{ label: "Java", value: "java", }, { label: "Kotlin", value: "kotlin" }];
 const BUILD_TOOLS = [{ label: "Maven", value: "maven", }, { label: "Gradle", value: "gradle" }];
 const JDK_VERSIONS = [{ label: "JDK 1.8", value: "1.8", }, { label: "JDK 11", value: "11" }];
+
+const ID_REGEXP = RegExp('^[A-Za-z0-9_\\-.]+$');
+const PACKAGE_NAME_REGEXP = RegExp('^[A-Za-z0-9_\\-.]+$');
 
 export interface Version {
 	number: string;
@@ -23,14 +27,13 @@ export interface StackItem {
 	description?: string;
 	items: VertxDependency[];
 }
-export interface Metdata {
+export interface Metadata {
 	defaults: any;
 	stack: StackItem[];
 	versions: Version[];
 	languages: string[];
 	jdkVersions: string[];
 	buildTools: string[];
-	vertxVersions: string[];
 }
 
 export function activate(context: vscode.ExtensionContext) {
@@ -42,17 +45,22 @@ export function activate(context: vscode.ExtensionContext) {
 
 async function createVertxProject() {
 	const metadata = await getMetadata();
-	const dependencies: { label: string; value: string; }[] = [];
-	metadata.stack.forEach((item) => {
-		item.items.forEach((dependency) => {
-			dependencies.push({ label: dependency.name, value: dependency.artifactId });
-		});
+	const versions = metadata.versions.map((it) => {
+		return {
+			value: it.number,
+			label: it.number,
+			exclusions: it.exclusions
+		};
 	});
 
 	const vertxVersion = await vscode.window.showQuickPick(
-		metadata.vertxVersions,
+		versions,
 		{ ignoreFocusOut: true, placeHolder: "Choose a Vert.x version" }
 	);
+	if (!vertxVersion) {
+		vscode.window.showErrorMessage("Impossible to create Vert.x Project: No Vert.x version provided.");
+		return;
+	}
 
 	const language = await vscode.window.showQuickPick(
 		LANGUAGES,
@@ -63,7 +71,6 @@ async function createVertxProject() {
 		}
 	});
 
-
 	const buildTool = await vscode.window.showQuickPick(
 		BUILD_TOOLS,
 		{ ignoreFocusOut: true, placeHolder: "Project a build tool" }
@@ -73,31 +80,36 @@ async function createVertxProject() {
 		}
 	});
 
+	const defaultGroupId = metadata.defaults.groupId;
 	const groupId = await vscode.window.showInputBox(
-		{ ignoreFocusOut: true, placeHolder: "Project GroupId" }
+		{
+			ignoreFocusOut: true,
+			placeHolder: "Your project group id",
+			value: defaultGroupId,
+			validateInput: (it) => ID_REGEXP.test(it) ? null : "GroupId invalid"
+		}
 	);
 	if (!groupId) {
-		vscode.window.showErrorMessage("Impossible to Create Vert.x Project: No groupId provided.");
+		vscode.window.showErrorMessage("Impossible to create Vert.x Project: No groupId provided.");
 		return;
 	}
 
+	const defaultArtifactId = metadata.defaults.artifactId;
 	const artifactId = await vscode.window.showInputBox(
-		{ ignoreFocusOut: true, placeHolder: "Project ArtifactId" }
+		{
+			ignoreFocusOut: true,
+			placeHolder: "Your project artifact id",
+			value: defaultArtifactId,
+			validateInput: (it) => ID_REGEXP.test(it) ? null : "ArtifactId invalid"
+		}
 	);
 	if (!artifactId) {
-		vscode.window.showErrorMessage("Impossible to Create Vert.x Project: No artifactId provided.");
+		vscode.window.showErrorMessage("Impossible to create Vert.x Project: No artifactId provided.");
 		return;
 	}
 
-	const vertxDependencies = await vscode.window.showQuickPick(
-		dependencies,
-		{ canPickMany: true, ignoreFocusOut: true, matchOnDetail: true, matchOnDescription: true, placeHolder: "Package Name" }
-	).then((picks) => {
-		if (picks && picks.push.length > 0) {
-			return picks.map((it) => it.value);
-		}
-		return [];
-	});
+	const exclusions = (vertxVersion.exclusions) ? vertxVersion.exclusions : [];
+	const vertxDependencies = await showDependenciesQuickPick(metadata.stack, exclusions);
 
 	let packageName = "";
 
@@ -107,7 +119,11 @@ async function createVertxProject() {
 	);
 	if (hasCustomPackageName && hasCustomPackageName === 'Yes') {
 		let customPackageName = await vscode.window.showInputBox(
-			{ ignoreFocusOut: true, placeHolder: "Package Name" }
+			{
+				ignoreFocusOut: true,
+				placeHolder: "Your project package name",
+				validateInput: (it) => PACKAGE_NAME_REGEXP.test(it) ? null : "Package name invalid"
+			}
 		);
 		if (customPackageName) {
 			packageName = customPackageName;
@@ -148,16 +164,87 @@ async function createVertxProject() {
 	let success = await vscode.commands.executeCommand('vscode.openFolder', projectDir, true);
 }
 
-async function getMetadata() {
+async function getMetadata(): Promise<Metadata> {
 	return await request.get(`${API_URL}/metadata`)
 		.then((body) => {
-			const metadata: Metdata = JSON.parse(body);
+			const metadata: Metadata = JSON.parse(body);
 			return metadata;
 		});
 }
 
 async function downloadProject(url: string, targetDir: vscode.Uri) {
 	return request.get(url, { headers: { 'User-Agent': 'vscode-vertx-starter' } }).pipe(unzipper.Extract({ path: targetDir.fsPath })).promise();
+}
+
+export interface VertxDependencyItem {
+	artifactId: string;
+	artifactName: string;
+	artifactDescription?: string;
+	categoryName: string;
+	categoryDescription?: string;
+}
+function toVertxDependencyItem(stack: StackItem[], exclusions: string[]): VertxDependencyItem[] {
+	const items: VertxDependencyItem[] = [];
+	stack.forEach((item) => {
+		item.items
+			.filter((dependency) => { return exclusions.indexOf(dependency.artifactId) === -1; })
+			.forEach((dependency) => {
+				items.push({
+					artifactId: dependency.artifactId,
+					artifactName: dependency.name,
+					artifactDescription: dependency.description,
+					categoryName: item.category,
+					categoryDescription: item.description
+				});
+			});
+	});
+	return items;
+}
+
+async function showDependenciesQuickPick(stack: StackItem[], exclusions: string[]): Promise<string[]> {
+	let selectedDependencies: VertxDependencyItem[] = [];
+	let unselectedDependencies: VertxDependencyItem[] = toVertxDependencyItem(stack, exclusions);
+	let current: any;
+	do {
+		//Build item list
+		let items = selectedDependencies.concat(unselectedDependencies).map((it) => {
+			return {
+				value: it.artifactId,
+				label: `${selectedDependencies.some((other) => it.artifactId === other.artifactId) ? '$(check) ' : ''}${it.artifactName}`,
+				description: it.categoryName,
+				detail: it.artifactDescription
+			};
+		});
+		//Push the dependencies selection stopper on top of the dependencies list
+		items.unshift({
+			value: 'stop',
+			label: `$(tasklist) ${selectedDependencies.length} dependencies selected`,
+			description: '',
+			detail: 'Press <Enter>  to continue'
+		});
+		current = await vscode.window.showQuickPick(
+			items,
+			{ ignoreFocusOut: true, matchOnDetail: true, matchOnDescription: true, placeHolder: "Vertx Dependencies" }
+		);
+		//When a dependency is picked toggle its status (selected/unselected)
+		if (current && current.value !== 'stop') {
+			//unselected
+			if (selectedDependencies.some((it) => { return it.artifactId === current.value; })) {
+				selectedDependencies = selectedDependencies.filter((it) => it.artifactId !== current.value);
+			} else {
+				//select
+				let dependency = unselectedDependencies.find(((it) => { return it.artifactId === current.value; }));
+				if (dependency) {
+					unselectedDependencies = unselectedDependencies.filter((it) => it.artifactId !== current.value);
+					selectedDependencies.push(dependency);
+				}
+			}
+
+		}
+	}
+	while (current && current.value !== 'stop');
+
+	return selectedDependencies.map((it) => it.artifactId);
 }
 
 export function deactivate() { }
