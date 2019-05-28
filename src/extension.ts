4da8e9665cf2f396d81f31dbd1af0417990b88bb
Daniel Petisme <daniel.petisme@gmail.com>
2019-05-28 23:05:52 +0200

    fix-2

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,28 @@
-import * as fs from "fs";
-import * as os from "os";
-import * as path from "path";
+'use strict';
+
+import * as fs from 'fs';
+import * as os from 'os';
+import * as path from 'path';
 import * as request from 'request-promise';
 import * as unzipper from 'unzipper';
 import * as vscode from 'vscode';
 
-const DEFAULT_API_URL = "https://start.vertx.io";
-const LANGUAGES = [{ label: "Java", value: "java", }, { label: "Kotlin", value: "kotlin" }];
-const BUILD_TOOLS = [{ label: "Maven", value: "maven", }, { label: "Gradle", value: "gradle" }];
-const JDK_VERSIONS = [{ label: "JDK 1.8", value: "1.8", }, { label: "JDK 11", value: "11" }];
+export interface QuickPickItemWithValue extends vscode.QuickPickItem {
+  value: string;
+}
+const DEFAULT_API_URL: string = 'https://start.vertx.io';
+const LANGUAGES: QuickPickItemWithValue[] = [{ label: 'Java', value: 'java', }, { label: 'Kotlin', value: 'kotlin' }];
+const BUILD_TOOLS: QuickPickItemWithValue[] = [{ label: 'Maven', value: 'maven', }, { label: 'Gradle', value: 'gradle' }];
+const JDK_VERSIONS: QuickPickItemWithValue[] = [{ label: 'JDK 1.8', value: '1.8', }, { label: 'JDK 11', value: '11' }];
 
-const CONFIG_SECTION_VERTX_STARTER = "vertx-starter";
-const CONFIG_DEFAULT_API_URL = "apiUrl";
-const CONFIG_DEFAULT_GROUP_ID = "defaultGroupId";
-const CONFIG_DEFAULT_ARTIFACT_ID = "defaultArtifactId";
-const CONFIG_DEFAULT_PACKAGE_NAME = "defaultPackageName";
+const CONFIG_SECTION_VERTX_STARTER: string = 'vertx-starter';
+const CONFIG_DEFAULT_API_URL: string = 'apiUrl';
+const CONFIG_DEFAULT_GROUP_ID:string = 'defaultGroupId';
+const CONFIG_DEFAULT_ARTIFACT_ID: string = 'defaultArtifactId';
+const CONFIG_DEFAULT_PACKAGE_NAME: string = 'defaultPackageName';
 
-const ID_REGEXP = RegExp('^[A-Za-z0-9_\\-.]+$');
-const PACKAGE_NAME_REGEXP = RegExp('^[A-Za-z0-9_\\-.]+$');
+const ID_REGEXP: RegExp = RegExp('^[A-Za-z0-9_\\-.]+$');
+const PACKAGE_NAME_REGEXP: RegExp = RegExp('^[A-Za-z0-9_\\-.]+$');
 
 export interface Version {
   number: string;
@@ -44,7 +49,7 @@ export interface Metadata {
 }
 
 export function activate(context: vscode.ExtensionContext) {
-  let disposable = vscode.commands.registerCommand("vertxStarter.createVertxProject", () => {
+  let disposable = vscode.commands.registerCommand('vertxStarter.createVertxProject', () => {
     createVertxProject();
   });
   context.subscriptions.push(disposable);
@@ -69,30 +74,26 @@ async function createVertxProject() {
     versions,
     {
       ignoreFocusOut: true,
-      placeHolder: "Choose a Vert.x version"
+      placeHolder: 'Choose a Vert.x version'
     }
   );
   if (!vertxVersion) {
-    vscode.window.showErrorMessage("Impossible to create Vert.x Project: No Vert.x version provided.");
+    vscode.window.showErrorMessage('Impossible to create Vert.x Project: No Vert.x version provided.');
     return;
   }
 
   const language = await vscode.window.showQuickPick(
     LANGUAGES,
-    { ignoreFocusOut: true, placeHolder: "Choose a language" }
-  ).then((it) => {
-    if (it) {
-      return it.value;
-    }
+    { ignoreFocusOut: true, placeHolder: 'Choose a language' }
+  ).then((item: QuickPickItemWithValue | undefined) => {
+    return (item) ? item.value : item;
   });
 
   const buildTool = await vscode.window.showQuickPick(
     BUILD_TOOLS,
-    { ignoreFocusOut: true, placeHolder: "Project a build tool" }
-  ).then((it) => {
-    if (it) {
-      return it.value;
-    }
+    { ignoreFocusOut: true, placeHolder: 'Project a build tool' }
+  ).then((item: QuickPickItemWithValue | undefined) => {
+    return (item) ? item.value : item;
   });
 
   let defaultGroupId = vscode.workspace.getConfiguration(CONFIG_SECTION_VERTX_STARTER).get<string>(CONFIG_DEFAULT_GROUP_ID);
@@ -102,13 +103,13 @@ async function createVertxProject() {
   const groupId = await vscode.window.showInputBox(
     {
       ignoreFocusOut: true,
-      placeHolder: "Your project group id",
+      placeHolder: 'Your project group id',
       value: defaultGroupId,
-      validateInput: (it) => ID_REGEXP.test(it) ? null : "GroupId invalid"
+      validateInput: (it: string) => ID_REGEXP.test(it) ? null : 'GroupId invalid'
     }
   );
   if (!groupId) {
-    vscode.window.showErrorMessage("Impossible to create Vert.x Project: No groupId provided.");
+    vscode.window.showErrorMessage('Impossible to create Vert.x Project: No groupId provided.');
     return;
   }
 
@@ -119,36 +120,36 @@ async function createVertxProject() {
   const artifactId = await vscode.window.showInputBox(
     {
       ignoreFocusOut: true,
-      placeHolder: "Your project artifact id",
+      placeHolder: 'Your project artifact id',
       value: defaultArtifactId,
-      validateInput: (it) => ID_REGEXP.test(it) ? null : "ArtifactId invalid"
+      validateInput: (it: string) => ID_REGEXP.test(it) ? null : 'ArtifactId invalid'
     }
   );
   if (!artifactId) {
-    vscode.window.showErrorMessage("Impossible to create Vert.x Project: No artifactId provided.");
+    vscode.window.showErrorMessage('Impossible to create Vert.x Project: No artifactId provided.');
     return;
   }
 
   const exclusions = (vertxVersion.exclusions) ? vertxVersion.exclusions : [];
   const vertxDependencies = await showDependenciesQuickPick(metadata.stack, exclusions);
 
-  let packageName = "";
+  let packageName = '';
 
   let hasCustomPackageName = await vscode.window.showQuickPick(
     ['No', 'Yes'],
-    { ignoreFocusOut: true, placeHolder: "Define a custom package Name?" }
+    { ignoreFocusOut: true, placeHolder: 'Define a custom package Name?' }
   );
   if (hasCustomPackageName && hasCustomPackageName === 'Yes') {
     let defaultPackageName = vscode.workspace.getConfiguration(CONFIG_SECTION_VERTX_STARTER).get<string>(CONFIG_DEFAULT_PACKAGE_NAME);
     if (!defaultPackageName) {
-      defaultPackageName = "";
+      defaultPackageName = '';
     }
     let customPackageName = await vscode.window.showInputBox(
       {
         ignoreFocusOut: true,
-        placeHolder: "Your project package name",
+        placeHolder: 'Your project package name',
         value: defaultPackageName,
-        validateInput: (it) => PACKAGE_NAME_REGEXP.test(it) ? null : "Package name invalid"
+        validateInput: (it: string) => PACKAGE_NAME_REGEXP.test(it) ? null : 'Package name invalid'
       }
     );
     if (customPackageName) {
@@ -158,18 +159,16 @@ async function createVertxProject() {
 
   const jdkVersion = await vscode.window.showQuickPick(
     JDK_VERSIONS,
-    { ignoreFocusOut: true, placeHolder: "Choose a JDK version" }
-  ).then((it) => {
-    if (it) {
-      return it.value;
-    }
+    { ignoreFocusOut: true, placeHolder: 'Choose a JDK version' }
+  ).then((item: QuickPickItemWithValue | undefined) => {
+    return (item) ? item.value : item;
   });
 
   let targetDir = await vscode.window.showOpenDialog(
     { canSelectFiles: false, canSelectFolders: true, canSelectMany: false }
   );
   if (!(targetDir && targetDir[0])) {
-    vscode.window.showErrorMessage("Impossible to Create Vert.x Project: No directory provided.");
+    vscode.window.showErrorMessage('Impossible to Create Vert.x Project: No directory provided.');
     return;
   }
   const projectDir = vscode.Uri.file(path.join(targetDir[0].fsPath, artifactId));
@@ -190,7 +189,7 @@ async function createVertxProject() {
 
   await downloadProject(projectUrl, projectDir);
 
-  let success = await vscode.commands.executeCommand('vscode.openFolder', projectDir, true);
+  await vscode.commands.executeCommand('vscode.openFolder', projectDir, true);
 }
 
 async function getMetadata(apiUrl: string): Promise<Metadata> {
@@ -253,7 +252,7 @@ async function showDependenciesQuickPick(stack: StackItem[], exclusions: string[
     });
     current = await vscode.window.showQuickPick(
       items,
-      { ignoreFocusOut: true, matchOnDetail: true, matchOnDescription: true, placeHolder: "Vertx Dependencies" }
+      { ignoreFocusOut: true, matchOnDetail: true, matchOnDescription: true, placeHolder: 'Vertx Dependencies' }
     );
     //When a dependency is picked toggle its status (selected/unselected)
     if (current && current.value !== 'stop') {
